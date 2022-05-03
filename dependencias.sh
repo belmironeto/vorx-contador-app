c38745e60a9aaab577336905c4835f491fee81ec
Belmiro Christo Neto <belmiro.neto@live.com>
2022-05-02 21:09:00 -0300

    Ajuste das dependencias

--- dependencias.sh
+++ dependencias.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo apt-get update -y
+sudo apt-get update
+sudo apt-get install git ansible -y
 
-git clone https://github.com/belmironeto/vorx-contador-app.git
+git clone https://github.com/belmironeto/vorx-contador-app.git /tmp/pointer-app/
+
+cd /tmp/pointer-app
+ansible-playbook pointer-playbook.yml
